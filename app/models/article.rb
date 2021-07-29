0db4b69cd16698bc6f4c16335bcd7a7f782a254a
David <truong.dohuy0501@gmail.com>
2021-07-29 14:25:49 +0700

    save point

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,32 +1,53 @@
 require 'elasticsearch/model'
 
 class Article < ApplicationRecord
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
+  # include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks
   searchkick
 
   has_many :posts,  dependent: :destroy
   validates :title, presence: true, uniqueness: true
   # validates :facts, presence: true
+  class << self
+    def query(params)
+      articles = self.active_record_search(params) # filter by other parameters first
 
-  def self.search(query)
-    __elasticsearch__.search(
-      {
-        query: {
-          multi_match: {
-            query: query&.first,
-            fields: ['title^10', 'text']
-          }
-        }
+      # return right there if search is blank
+
+      # otherwise pass already filtered set to elastic search for further filtering
+      article_ids = articles.pluck(:id)
+      self.elastic_search(params, article_ids)
+    end
+
+    def elastic_search(params, listing_ids)
+      elastic_query = {
+        query: query&.first,
+        fields: ['title^10', 'text']
       }
-    )
-  end
-  settings index: { number_of_shards: 1 } do
-    mappings dynamic: 'false' do
-      indexes :title, analyzer: 'english'
-      indexes :text, analyzer: 'english'
+  
+      self.search(params[:search], elastic_query)
     end
   end
-  Article.__elasticsearch__.create_index!
-  Article.import # for auto sync model with elastic search
+
+
+  # def self.search(query)
+  #   __elasticsearch__.search(
+  #     {
+  #       query: {
+  #         multi_match: {
+  #           query: query&.first,
+  #           fields: ['title^10', 'text']
+  #         }
+  #       }
+  #     }
+  #   )
+  # end
+  # settings index: { number_of_shards: 1 } do
+  #   mappings dynamic: 'false' do
+  #     indexes :title, analyzer: 'english'
+  #     indexes :text, analyzer: 'english'
+  #   end
+  # end
+  # Article.__elasticsearch__.create_index!
+  # Article.import # for auto sync model with elastic search
 end
